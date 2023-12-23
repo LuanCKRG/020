8bb8a40b614020b6ede85e5caf61a654978cf30f
Luan Vitor <luancamposck@gmail.com>
2023-12-23 09:31:07 +0000

    Update 3 files

--- src/lib/supabase/auth.ts
+++ src/lib/supabase/auth.ts
@@ -5,6 +5,22 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { Database } from "@/types/supabase";
 import { redirect } from "next/navigation";
 
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiG6scpd3bzCeCBiv60-Z-gQeEX1tc03s",
+  authDomain: "yield-master.firebaseapp.com",
+  projectId: "yield-master",
+  storageBucket: "yield-master.appspot.com",
+  messagingSenderId: "793696024570",
+  appId: "1:793696024570:web:46fec669b25c5618ad5643",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 const cookieStore = cookies();
 
 const supabase = createServerClient<Database>(
@@ -70,20 +86,28 @@ export const gogole = async () => {
 };
 
 export const signinGoogle = async () => {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      queryParams: {
-        user_name: "name",
-        email: "email",
-      },
-    },
-  });
-
-  console.log(data);
-  console.error("error" + error);
-
-  return redirect(data.url!);
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 };
 
 export const signOut = async () => {
